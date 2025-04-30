0aa8065dca2d8dffb963293b3e72ae52e54cbc09
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2025-04-30 12:17:41 -0400

    mode dark and light

--- src/app/country/components/top-menu/top-menu.component.ts
+++ src/app/country/components/top-menu/top-menu.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Theme, ThemeService } from '../../../services/theme.service';
 
 @Component({
   selector: 'country-top-menu',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './top-menu.component.html',
 })
-export class TopMenuComponent { }
+export class TopMenuComponent {
+  themeService = inject(ThemeService);
+
+  isLight = model(this.themeService.getCurrentTheme()() === 'light');
+}
